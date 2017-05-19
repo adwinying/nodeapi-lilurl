da17911fbb850a1ef02c485fec6667527082cb7a
Adwin Ying <adwinying@gmail.com>
2017-05-20 03:15:31 +0900

    Fix url conflicts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path        = require('path')
+const favicon			= require('serve-favicon')
 const express     = require('express')
 const mongoClient = require('mongodb').MongoClient
 
@@ -8,6 +9,8 @@ const dbURL = `mongodb://${process.env.LILURL_USER}:${process.env.LILURL_PW}@ds1
 
 //Set static page
 app.use(express.static(path.join(__dirname, 'public')))
+//Set favicon
+app.use(favicon(path.join(__dirname, 'public/img/favicon.ico')))
 
 function getUniqueNum(collection, callback) {
 	let randNum = Math.floor(Math.random() * 9000 + 1000)
@@ -27,11 +30,25 @@ function getUniqueNum(collection, callback) {
 	})
 }
 
-//Create new link
-app.get('/new/*', (req, res) => {
-	let oriURL = req.params[0].match(/^http(s?):\/\/.+\..+/)
+//Route pages
+app.get('/*', (req, res) => {
+	console.log('req.params:', req.params[0])
 
-	if (oriURL) {
+	if (req.params[0].match(/^new\/.+/)) {
+		newLinkController(req, res)
+	} else {
+		getLinkController(req, res)
+	}
+})
+
+//Create new link
+const newLinkController = (req, res) => {
+	let url = req.params[0].match(/^new\/https?:\/\/.+\..+/)
+
+	console.log('store url:', url)
+
+	if (url) {
+		let oriURL = url[0].replace('new/', '')
 		
 		mongoClient.connect(dbURL, (err, db) => {
 			if (err) throw err
@@ -40,14 +57,14 @@ app.get('/new/*', (req, res) => {
 			let isNumUnique = false
 
 			getUniqueNum(collection, (err, uniqueNum) => {
-				collection.insertOne({urlid: uniqueNum, oriURL: oriURL[0]}, (err, insResults) => {
+				collection.insertOne({urlid: uniqueNum, oriURL: oriURL}, (err, insResults) => {
 					if (err) throw err
 					
-					console.log('DB insert successful!')
+					console.log('DB insert successful!\n')
 					db.close()
 					
 					res.json({
-						originalURL: oriURL[0],
+						originalURL: oriURL,
 						lilURL: `http://lilurl.nodejs.iadw.in/${uniqueNum}`
 					})
 				})
@@ -60,37 +77,39 @@ app.get('/new/*', (req, res) => {
 		})
 	}
 
-})
+}
 
 //Redirect to original link if found
-app.get('/:urlid', (req, res) => {
-	let urlid = parseInt(req.params.urlid)
+const getLinkController = (req, res) => {
+	let urlid = parseInt(req.params[0])
 
-	console.log(urlid)
+	console.log('get oriURL:', urlid)
 	
-	mongoClient.connect(dbURL, (err, db) => {
-		if (err) throw err
-
-		let collection  = db.collection('urls')
-
-		collection.findOne({'urlid': urlid}, (err, findResults) => {
+	if (!isNaN(urlid)) {
+		mongoClient.connect(dbURL, (err, db) => {
 			if (err) throw err
 
-			db.close()
+			let collection  = db.collection('urls')
 
-			if (findResults) {
-				console.log('urlid found. Redirecting to', findResults.oriURL)
-				res.redirect(findResults.oriURL)
-			} else {
-				console.log('urlid not found')
-				res.json({
-					error: 'URL not found'
-				})
-			}
+			collection.findOne({'urlid': urlid}, (err, findResults) => {
+				if (err) throw err
+
+				db.close()
+
+				if (findResults) {
+					console.log('urlid found. Redirecting to', findResults.oriURL, '\n')
+					res.redirect(findResults.oriURL)
+				} else {
+					console.log('urlid not found \n')
+					res.json({
+						error: 'URL not found'
+					})
+				}
+			})
 		})
-	})
+	}
+}
 
-})
 
 app.listen(port)
 console.log('Server started at port', port, '\n')
